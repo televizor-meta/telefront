86e5d69ad2aeb442119eef53b18346e8d895aead
orphey <kaletvintsev@gmail.com>
2021-12-04 04:03:33 +0300

    upd

--- src/apps/simple_user/avatar/Avatar.vue
+++ src/apps/simple_user/avatar/Avatar.vue
@@ -2,10 +2,13 @@
       <div :class="{
             'avatar__bg': true,
             'avatar__bg__m': size === 'm',
-            'avatar__bg__xl': size === 'xl'
+            'avatar__bg__xl': size === 'xl',
            }">
-        <img :src="require('../../../assets/images/Ellipse 1.svg')" alt="">
-        <div id="avatar"></div>
+        <img class="rotate ghost" :src="require('../../../assets/images/Ellipse 1.svg')" alt="">
+        <div id="avatar">
+          <h2>{{animationState}}</h2>
+          <img :src="require(`../../../assets/images/avatars/boy/sprite${this.animationState}.png`)"  alt="ghost">
+        </div>
       </div>
 </template>
 
@@ -15,12 +18,46 @@ export default {
   props: {
     size: {
       type: String,
-      default: 'm'
+      default: 'm',
     },
   },
+  data(){
+    return{
+      polling: null,
+      animationState: 2,
+    }
+  },
+  methods: {
+        tickAnimation () {
+
+            this.polling = setInterval(() => {
+                if (this.animationState < 20){
+                  if (this.animationState === 12){
+                    this.animationState += 2
+                    clearInterval(this.polling)
+                    setTimeout(this.tickAnimation, 1600)
+                  }else if (this.animationState === 19){
+                    this.animationState += 1
+                    clearInterval(this.polling)
+                    setTimeout(this.tickAnimation, 400)
+                  }
+                  else {
+                    this.animationState += 1
+                  }
+                } else {
+                  this.animationState = 2
+                }
+            }, 150)
+        }
+    },
+  beforeDestroy () {
+      clearInterval(this.polling)
+  },
+  created () {
+      this.tickAnimation()
+  }
 }
 </script>
 
-<style scoped>
-
+<style >
 </style>
